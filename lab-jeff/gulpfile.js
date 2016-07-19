adb7f2a587eec8ad5d11ba6bb83b2fd229181703
Jeffrey Gebhardt <jeff@gebbey.com>
2016-07-19 12:31:06 -0700

    added readme

--- lab-jeff/gulpfile.js
+++ lab-jeff/gulpfile.js
@@ -1,15 +1,9 @@
 const gulp = require('gulp');
-const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
 
-gulp.task('lint', () => {
+gulp.task('default', () => {
   return gulp.src(['**/*.js', '!node_modules/**'])
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
-});
-
-gulp.task('default', ['lint'], () => {
-  return gulp.src('./test/test.js')
-  .pipe(mocha({reporter: 'spec'}));
 });
